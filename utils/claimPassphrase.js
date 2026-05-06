4055ca55f6084b08db724c34f0ab8ce3fc86d3fb
mira <mira-chan1@outlook.com>
2026-05-06 15:45:24 +0800

    feat: update CLAIM_COLOR_OPTIONS with a comprehensive color list and improved organization

--- utils/claimPassphrase.js
+++ utils/claimPassphrase.js
@@ -1,7 +1,16 @@
-const CLAIM_COLOR_OPTIONS = ["indigo", "magenta", "seagrass", "lightsalmon", "darksalmon", "lightcoral",
-	 "indianred","firebrick", "darkred", "orangered", "darkorange", "lightyellow", "lemonchiffon", 
-	 "lightgoldenrodyellow", "papayawhip", "moccasin", "mediumspringgreen", "mediumseagreen", "mediumaquamarine", "paleturquoise", "darkolivegreen", "cornflowerblue",
-	  "midnightblue", "deeppink", "hotpink", "floralwhite", "oldlace", "beige", "gainsboro", "rosybrown", "sienna", "maroon", "tan"];
+const CLAIM_COLOR_OPTIONS = [
+	"lightsalmon", "salmon", "darksalmon", "lightcoral", "indianred", "crimson", "firebrick", "red", "darkred", // red
+	"coral", "tomato", "orangered", "gold", "orange", "darkorange", // orange
+	"lightyellow", "lemonchiffon", "lightgoldenrodyellow", "papayawhip", "moccasin", "peachpuff", "	palegoldenrod", "khaki", "darkkhaki", "yellow", // yellow
+	"lawngreen", "chartreuse", "limegreen", "lime", "forestgreen", "green", "darkgreen", "greenyellow", "yellowgreen", "springgreen", "mediumspringgreen", "lightgreen", "palegreen", "darkseagreen", "mediumseagreen", "seagreen", "olive", "darkolivegreen", "olivedrab", // green
+	"lightcyan", "cyan", "aqua", "aquamarine", "mediumaquamarine", "paleturquoise", "turquoise", "mediumturquoise", "darkturquoise", "	lightseagreen", "cadetblue", "darkcyan", "teal", // cyan
+	"powderblue", "lightblue", "lightskyblue", "skyblue", "deepskyblue", "lightsteelblue", "dodgerblue", "cornflowerblue", "steelblue", "royalblue", "blue", "mediumblue", "darkblue", "navy", "midnightblue", "mediumslateblue", "slateblue", "darkslateblue", // blue
+	"lavender", "thistle", "plum", "violet", "orchid", "fuchsia", "magenta", "mediumorchid", "mediumpurple", "blueviolet", "darkviolet", "darkorchid", "darkmagenta", "purple", "indigo", // purple
+	"pink", "lightpink", "hotpink", "deeppink", "palevioletred", "mediumvioletred", // pink
+	"white", "snow", "honeydew", "mintcream", "azure", "aliceblue", "ghostwhite", "whitesmoke", "seashell", "beige", "oldlace", "floralwhite", "ivory", "antiquewhite", "linen", "lavendarblush", "mistyrose", // white
+	"gainsboro", "lightgray", "silver", "darkgray", "gray", "dimgray", "lightslategray", "slategray", "darkslategray", "black", // gray
+	"cornsilk", "blanchedalmond", "bisque", "navajowhite", "wheat", "burlywood", "tan", "rosybrown", "sandybrown", "goldenrod", "peru", "chocolate", "saddlebrown", "sienna", "brown", "maroon" // brown
+];
 
 const normalizeClaimColor = (value) => {
 	if (typeof value !== "string") return "";
