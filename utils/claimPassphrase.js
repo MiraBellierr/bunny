bc656d645fb0db3eac98f8e6ed2bcb2b1859196f
mira <mira-chan1@outlook.com>
2026-05-06 23:40:37 +0800

    feat: fix typo in CLAIM_COLOR_OPTIONS array

--- utils/claimPassphrase.js
+++ utils/claimPassphrase.js
@@ -7,7 +7,7 @@ const CLAIM_COLOR_OPTIONS = [
 	"powderblue", "lightblue", "lightskyblue", "skyblue", "deepskyblue", "lightsteelblue", "dodgerblue", "cornflowerblue", "steelblue", "royalblue", "blue", "mediumblue", "darkblue", "navy", "midnightblue", "mediumslateblue", "slateblue", "darkslateblue", // blue
 	"lavender", "thistle", "plum", "violet", "orchid", "fuchsia", "magenta", "mediumorchid", "mediumpurple", "blueviolet", "darkviolet", "darkorchid", "darkmagenta", "purple", "indigo", // purple
 	"pink", "lightpink", "hotpink", "deeppink", "palevioletred", "mediumvioletred", // pink
-	"white", "snow", "honeydew", "mintcream", "azure", "aliceblue", "ghostwhite", "whitesmoke", "seashell", "beige", "oldlace", "floralwhite", "ivory", "antiquewhite", "linen", "lavendarblush", "mistyrose", // white
+	"white", "snow", "honeydew", "mintcream", "azure", "aliceblue", "ghostwhite", "whitesmoke", "seashell", "beige", "oldlace", "floralwhite", "ivory", "antiquewhite", "linen", "lavenderblush", "mistyrose", // white
 	"gainsboro", "lightgray", "silver", "darkgray", "gray", "dimgray", "lightslategray", "slategray", "darkslategray", "black", // gray
 	"cornsilk", "blanchedalmond", "bisque", "navajowhite", "wheat", "burlywood", "tan", "rosybrown", "sandybrown", "goldenrod", "peru", "chocolate", "saddlebrown", "sienna", "brown", "maroon" // brown
 ];
